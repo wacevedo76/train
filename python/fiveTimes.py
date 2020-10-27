0dc65c4793fa242828544f7a9544939cb6df0161
William Acevedo <william.acevedo@gmail.com>
2020-10-27 13:35:41 +0100

    Removed python dir

--- python/fiveTimes.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3
-
-print('My name is')
-for i in range(5):
-    print('Jimmy Five Times (' + str(i) + ')')
--- python/yourName.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-
-name = ''
-while name != 'your name':
-    print('Please print your name.')
-    name = input()
-
-print('Thank you!')
