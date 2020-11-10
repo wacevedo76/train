36818f9ab84abedea821327286365896808bff5d
William Acevedo <william.acevedo@gmail.com>
2020-11-10 08:40:27 -0500

    Add javascript countdown project

---
+++ javascript/kata/clocks/index.js
@@ -0,0 +1,1 @@
+const clocks = require('./lib/clocks');
---
+++ javascript/kata/clocks/lib/clocks.js
@@ -0,0 +1,8 @@
+class clock {
+  constructor(hour, minute, second) {
+    this.hour = hour;
+    this.minute = minute
+    this.second = second 
+  }
+  // Sanity checks froj
+}
