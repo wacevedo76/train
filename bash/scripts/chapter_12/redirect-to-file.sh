b9739d70278c3dbbb4d183c52e712f072dd19ba8
William Acevedo <william.acevedo@gmail.com>
2020-12-28 10:27:35 +0100

    Adds redirect-to-file to pracitce redirects

--- bash/scripts/chapter_12/redirect-to-file.sh
+++ bash/scripts/chapter_12/redirect-to-file.sh
@@ -2,14 +2,17 @@
 
 ################################################################################
 # Author: Sebastiaan Tammer
-# Version: 1.0.0
+# Version: 1.1.0
 # Date: Mon Dec 28 2020 
 # Description: Redirect user input to file.
 # Usage: .redrect-to-file.sh
 ################################################################################
 
+# since we're dealing with paths, set current working directory.'
+cd $(dirname $0)
+
 # capture the users' input.
 read -p "Type anything you like: " user_input
 
 #save the user's input to a file.
-echo ${user_input} > redirect-to-file.txt
+echo ${user_input} >> redirect-to-file.txt
