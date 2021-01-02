0bb2db7d125b72090edcf7573dd93020e4674f18
William Acevedo <william.acevedo@gmail.com>
2021-01-02 13:30:35 +0100

    Adds scripts to practice case switch statement and logical defaults

---
+++ bash/scripts/chapter_13/error-functions.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+################################################################################
+# Author: Sebastiaan Tammer
+# Version: v1.0.0
+# Date: Wed Dec 30 2020 
+# Description: Functions to handle errors.
+# Usage:  ./error-functions.sh
+################################################################################
+
+# Define a function that handles minor errors.
+handle_minor_error() {
+  echo "A minor error has occured, please check the output."
+}
+
+# Define a function that handles fatal errors.
+handle_fatal_error() {
+  echo "A critical error has occured, stopping script."
+  exit 1
+}
+
+# Minor failures.
+ls -l /tmp/ || handle_minor_error
+ls -l /root || handle_minor_error
+
+# Fatal failures.
+cat /etc/shadow || handle_fatal_error
+cat /etc/passwd || handle_fatal_error
