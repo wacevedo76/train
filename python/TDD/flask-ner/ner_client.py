f8c849f9dbaf4e58fd72bbd21c8fdf7440b8f721
William Acevedo <william.acevedo@gmail.com>
2021-01-09 18:24:16 +0100

    Creates and starts flask NER client and test file

---
+++ python/TDD/flask-ner/ner_client.py
@@ -0,0 +1,8 @@
+import spacy
+
+class NamedEntityClient:
+    def __init__(self, model):
+        self.model = model
+
+    def get_ents(self, sentence):
+        return {}
---
+++ python/TDD/flask-ner/test/test_ner_client.py
@@ -0,0 +1,17 @@
+import unittest
+from ner_client import NamedEntityClient
+
+class TestNerClient(unittest.TestCase):
+    # { ents: [{...}]},
+    #   html: "<span>..."}
+
+    def test_get_ents_returns_dictionary_given_empty_input(self):
+        model = NerModelTestDouble('eng')
+        ner = NamedEntityClient(model)
+        ents = ner.get_ents("")
+        self.assertIsInstance(ents, dict)
+
+    def test_get_ents_returns_list_given_nonempty_string(self):
+        ner = NamedEntityClient(model)
+        ents = ner.get_ents("Madison is a city in Wiscon")
+        self.assertIsInstance(ents, dict)
