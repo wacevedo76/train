3687c3adeee80249a3de16a52b28f565602ababa
William Acevedo <william.acevedo@gmail.com>
2021-04-05 23:15:35 +0200

    Restared learning c -adds 'The C programming Language dir ch01 dir

---
+++ C/The C Programming Language/ch01/fahToCel.c
@@ -0,0 +1,29 @@
+/*
+  Author: 
+  Version: v1.1.0
+  Date: Outputs a table of Fahrenheit to Celsius conversions
+  Description: 
+  Usage: fatToCel
+*/
+
+#include <stdio.h>
+
+int main()
+{
+  int fahr, celsius;
+  int lower, upper, step;
+
+  lower = 0;          /* lower limits of temperature table */
+  upper = 300;        /* upper limit */
+  step = 20;
+
+  fahr = lower;
+  while (fahr <= upper) {
+    celsius = 5 * (fahr-32) / 9;
+    printf("%3d  %6d\n", fahr, celsius);
+    fahr = fahr + step;
+  }
+
+  return 0;
+}
+
---
+++ C/The C Programming Language/ch01/hello.c
@@ -0,0 +1,16 @@
+/*
+# Author: 
+# Version: v1.0.0
+# Date: 
+# Description: 
+# Usage: 
+*/
+
+#include <stdio.h>
+
+int main()
+{
+  printf("Hello, World!\n");
+  return 0;
+}
+
