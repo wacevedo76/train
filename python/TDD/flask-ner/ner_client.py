6acaed5c56fbfca33101e04c9d97afe719924669
William Acevedo <william.acevedo@gmail.com>
2021-05-25 06:30:40 +0200

    Reset repo to head, started again

--- python/TDD/flask-ner/ner_client.py
+++
@@ -1,8 +0,0 @@
-import spacy
-
-class NamedEntityClient:
-    def __init__(self, model):
-        self.model = model
-
-    def get_ents(self, sentence):
-        return {}
--- python/TDD/flask-ner/test/test_ner_client.py
+++ python/TDD/flask-ner/test/test_ner_client.py
@@ -1,19 +1,8 @@
 import unittest
-from ner_client import NamedEntityClient
 
 class TestNerClient(unittest.TestCase):
 
-    # { ents: [{...}],
-    #   html: "<span>..."}
-
     def test_get_ents_returns_dictionary_given_empty_string(self):
-        model = NerModelTestDouble('eng')
-        ner = NamedEntityClient(model)
+        ner = NamedEntityClient()
         ents = ner.get_ents("")
         self.assertIsInstance(ents, dict)
-
-    def test_get_ents_returns_list_given_nonempty_string(self):
-        ner = NamedEntityClient(model)
-        ents = ner.get_ents("Madison is a city in Wisconsin")
-        self.assertIsInstance(ents, dict)
-
