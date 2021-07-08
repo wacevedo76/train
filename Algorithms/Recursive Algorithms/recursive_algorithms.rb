5b4528194e4a75f8080e919d1a09d4e9d2bc7c75
William Acevedo <william.acevedo@gmail.com>
2021-07-08 20:18:15 +0200

    makes Astetic changes, removes test case

--- Algorithms/Recursive Algorithms/recursive_algorithms.rb
+++ Algorithms/Recursive Algorithms/recursive_algorithms.rb
@@ -1,4 +1,10 @@
-# takes each idivitual diggit and adds them (recursively) until only one digit remains
+# takes each individual digit and adds them (recursively) until only one digit remains
 def sum_of_digits(n)
-  n < 0 ? 'Enter a positive number" : n < 9 ? n : sum_of_digits((n / 10) + (n % 10))
+  n < 0 ? 'Enter a positive integer' : n < 9 ? n : sum_of_digits((n / 10) + (n % 10))
 end
+
+def factorial(n)
+  n < 0 ? 'Enter a positive integer' : n <= 1 ? 1 : n * factorial(n - 1)
+end
+
+
