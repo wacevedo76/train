59d15d6d3cd8ad73314321021d7427f3d48955e9
William Acevedo <william.acevedo@gmail.com>
2021-07-11 18:56:21 +0200

    Initial commit, Cards

---
+++ go/Go course/cards/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	// cards := []string{"Ace of Diamonds", newCard()}
+	cards := newDeck()
+
+	hand, remainingCards := deal(cards, 5)
+
+	hand.print()
+	fmt.Println("------------------------------------------")
+	remainingCards.print()
+}
--- go/Go course/main.go
+++
@@ -1,8 +0,0 @@
-package main
-
-import "fmt"
-
-func main() {
-	var card string = "Ace of Spades"
-	fmt.Println(card)
-}
