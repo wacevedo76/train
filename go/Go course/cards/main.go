8992f029f344f2c3c51c2b939f283fd79ab399a6
William Acevedo <william.acevedo@gmail.com>
2021-07-12 00:19:12 +0200

    Adds several new helper functions

--- go/Go course/cards/main.go
+++ go/Go course/cards/main.go
@@ -1,14 +1,6 @@
 package main
 
-import "fmt"
-
 func main() {
-	// cards := []string{"Ace of Diamonds", newCard()}
-	cards := newDeck()
-
-	hand, remainingCards := deal(cards, 5)
-
-	hand.print()
-	fmt.Println("------------------------------------------")
-	remainingCards.print()
+	myDeck := newDeckFromFile("myFile")
+	myDeck.print()
 }
